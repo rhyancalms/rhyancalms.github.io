f48fbee9e6cef43b10ff403c6965b216aeca66b3
rhyancalms <calam.rhyan@gmail.com>
2023-02-10 10:02:42 -0800

    initial commit

--- app.js
+++ app.js
@@ -23,12 +23,3 @@ menu_item.forEach((item) => {
         mobile_menu.classList.toggle('active');
     });
 });
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior : "smooth"
-        });
-    });
-});
