6bd4a96b2b4023a36bc55d21435affb9a7727ae8
rhyancalms <calam.rhyan@gmail.com>
2023-02-10 01:35:42 -0800

    Added transition

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
+
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = "#29323c";
+    }else{
+        header.style.backgroundColor = "transparent";
+    }
+})
